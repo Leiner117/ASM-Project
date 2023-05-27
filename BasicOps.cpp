1dbc95ed551bfadc8dd0e793ea0f284ed70a5c6d
Leiner117 <lealvarado@estudiantec.cr>
2023-05-26 22:24:25 -0600

    add factorial function and decimalBinario(No funciona)

--- BasicOps.cpp
+++ BasicOps.cpp
@@ -6,7 +6,7 @@
 #include <time.h>
 #include <stdlib.h>
 #include <unistd.h>
-
+#include <cstdio>
 using namespace std;
 
 /// Estas son las llamadas de todos los menus
@@ -17,20 +17,38 @@ void menuDivision();
 void menuPotencia();
 void menuCalculadora();
 void menuPrincipal();
-
+void hora();
+void factorial();
+void decimalBinario();
 ///----------------------------------------Menu principal------------------------------------------------///
 
 
 void menuPrincipal(){
+    int op = 0;
     cout << "1. Consultar la hora del sistema"<<endl;
     cout << "2. Calculadora"<<endl;
     cout << "3. Calcular el factorial"<<endl;
     cout << "4. Conversion de unidades"<<endl;
     cout << "5. Temperatura"<< endl;
 
+    cin>> op;
+    if (op == 1){
+        hora();
+    }
+    if (op == 3){
+        factorial();
+    }
 }
 
+void menuCOnversiones(){
+    int op = 0;
+    cout<< "1. Decimal a Binario"<<endl;
+    cout<< "2. Binario a Decimal"<<endl;
+    if (op == 1){
+        decimalBinario();
+    }
 
+}
 ///----------------------------------------Operaciones Aritmeticas------------------------------------------------///
 
 void menuCalculadora(){
@@ -195,22 +213,115 @@ void menuDivision(){
 }
 
 void hora(){
-    char formato[] = "%02d:%02d:%02d\n";
-    int hora, minuto, segundo;
 
-    asm(
-            "mov $0, %%eax \n"
-            "leal %1, %%ebx \n"
-            "xor %%ecx, %%ecx \n"
-            "xor %%edx, %%edx \n"
-            "int $0x80 \n"
-            : "=c" (hora), "=d" (minuto), "=S" (segundo)
-            : "b" (formato)
-            : "%eax"
+    int hour, minute, second;
+
+    __asm__ volatile (
+            "movl $10, %%ebx\n"             // Función 10: sys_gettimeofday
+            "xorl %%ecx, %%ecx\n"           // Puntero a estructura timeval en ecx
+            "xorl %%edx, %%edx\n"           // Puntero a estructura timezone en edx
+            "int $0x80\n"                   // Llamada al sistema
+
+            "movl %%eax, %0\n"              // Almacenar el valor de retorno en hour
+            "movl 0x4(%%ecx), %1\n"          // Almacenar tv_sec en minute
+            "movl 0x8(%%ecx), %2\n"          // Almacenar tv_usec en second
+            : "=r" (hour), "=r" (minute), "=r" (second)
+            :
+            : "%eax", "%ebx", "%ecx", "%edx"
             );
+
+    std::cout << hour << ":" << minute << ":" << second << std::endl;
+
+}
+
+int factorial_asm(int n) {
+    int result;
+
+
+    __asm__ __volatile__(
+            "movl $1, %%eax\n"
+            "movl %1, %%ecx\n"
+            "loop_start:\n"
+            "imull %%ecx, %%eax\n"
+            "decl %%ecx\n"
+            "cmpl $1, %%ecx\n"
+            "jg loop_start\n"
+            : "=a" (result)
+            : "c" (n)
+            :
+            );
+
+    return result;
+}
+
+void factorial() {
+    int num;
+    std::cout << "Ingrese un número: ";
+    std::cin >> num;
+
+    int result = factorial_asm(num);
+
+    std::cout << "El factorial de " << num << " es: " << result << std::endl;
+
+
 }
 
 
+
+
+void decimalToBinary_asm(int n) {
+    __asm__ __volatile__(
+            "pushl %%ebp\n"
+            "movl %%esp, %%ebp\n"
+            "subl $4, %%esp\n"
+            "movl %0, %%eax\n"
+            "movl $0, %%ebx\n"
+            "movl $0, %%ecx\n"
+
+            "convert_loop:\n"
+            "cmpl $0, %%eax\n"
+            "je loop_end\n"
+            "movl $2, %%ebx\n"
+            "xorl %%edx, %%edx\n"
+            "divl %%ebx\n"
+            "movl %%edx, (%%ecx)\n"
+            "incl %%ecx\n"
+            "jmp convert_loop\n"
+
+            "loop_end:\n"
+            "cmpl $0, %%ecx\n"
+            "je print_zero\n"
+
+            "print_loop:\n"
+            "decl %%ecx\n"
+            "movl (%%ecx), %%eax\n"
+            "addl $48, %%eax\n"
+            "pushl %%eax\n"
+            "call putchar\n"
+            "addl $4, %%esp\n"
+            "cmpl $0, %%ecx\n"
+            "jg print_loop\n"
+
+            "print_zero:\n"
+            "pushl $48\n"
+            "call putchar\n"
+            "addl $4, %%esp\n"
+
+            "movl %%ebp, %%esp\n"
+            "popl %%ebp\n"
+            :
+            : "g" (n)
+            : "%eax", "%ebx", "%ecx", "%edx"
+            );
+}
+
+void decimalBinario(){
+    int num;
+    std::cout << "Ingrese un número decimal: ";
+    std::cin >> num;
+
+    decimalToBinary_asm(num);
+}
 
 int main() {
     menuPrincipal();
